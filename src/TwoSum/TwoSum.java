479581d4a7857fad56f18cd2ccc28f7950dc3cbe
umr006 <umar.ausheff@yandex.ru>
2024-10-08 17:49:13 +0500

    second

--- src/TwoSum/TwoSum.java
+++ src/TwoSum/TwoSum.java
@@ -4,23 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         Map<Integer, Integer> map = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-
             int diff = target - nums[i];
-            
             if (map.containsKey(Math.abs(diff))) {
-                result = new int[] {map.get(diff), i};
+                result = new int[] {map.get(Math.abs(diff)), i};
             }
-
             map.put(nums[i], i);
-
         }
-
         return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {2,17,11,15,18,6,7};
+        int target = 9;
+        int res[] = twoSum(nums, target);
+        System.out.println(res[0] + " " + res[1]);
     }
 
 }
